12ef87c8c191138cffb7c82bea2a5039fd6582af
Bruno <brunoarrumadordecarros@hotmail.com>
2019-03-28 15:48:49 +0000

    [UPDATE]  Video playing with image placeholder

--- OLXSwift/OLXSwift/Controller/Detail/DetailViewController.swift
+++ OLXSwift/OLXSwift/Controller/Detail/DetailViewController.swift
@@ -4,21 +4,32 @@ import youtube_ios_player_helper_swift
 class DetailViewController: UIViewController {
 
     var resource: Response.Resource!
-
-    @IBOutlet private weak var playerView: YTPlayerView!
+    private var playerView: PlayerView = PlayerView.fromNib()
+    
     @IBOutlet private weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = self.resource.title
-//        self.imageView.loadImageUsingUrlString(urlString: self.resource.thumbnails.large.url)
+        self.imageView.loadImageUsingUrlString(urlString: self.resource.thumbnails.large.url)
         self.loadWebView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        addPlayerView()
+    }
+    
+    private func addPlayerView(){
+        self.view.addSubview(playerView)
+        playerView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 240)
+        playerView.autoresizingMask = .flexibleWidth
     }
 
     func loadWebView() {
-        if  let youtubeId = resource?.videoEmbed.components(separatedBy: "/").last {
-            _ = playerView.load(videoId: youtubeId)
+        if let youtubeId = resource?.videoEmbed.components(separatedBy: "/").last {
+            playerView.videoId = youtubeId
         }
     }
 
