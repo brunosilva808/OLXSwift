bc564cceca5cf9f72cfa41bd686cb58e1ca0c618
Bruno <brunoarrumadordecarros@hotmail.com>
2019-03-28 17:28:28 +0000

    [UPDATE] update video and image frames

--- OLXSwift/OLXSwift/Controller/Detail/DetailViewController.swift
+++ OLXSwift/OLXSwift/Controller/Detail/DetailViewController.swift
@@ -5,15 +5,27 @@ class DetailViewController: UIViewController {
 
     var resource: Response.Resource!
     private var playerView: PlayerView = PlayerView.fromNib()
+    @IBOutlet weak var topImageConstraint: NSLayoutConstraint!
     
     @IBOutlet private weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.title = self.resource.title
-        self.imageView.loadImageUsingUrlString(urlString: self.resource.thumbnails.large.url)
+        self.setupView()
         self.loadWebView()
+    }
+    
+    func setupView() {
+        self.title = self.resource.title
+        self.view.backgroundColor = .black
+        self.imageView.loadImageUsingUrlString(urlString: self.resource.thumbnails.large.url)
+    }
+    
+    func loadWebView() {
+        if let youtubeId = resource?.videoEmbed.components(separatedBy: "/").last {
+            playerView.videoId = youtubeId
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -23,14 +35,10 @@ class DetailViewController: UIViewController {
     
     private func addPlayerView(){
         self.view.addSubview(playerView)
-        playerView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 240)
+        
+        self.topImageConstraint.constant = self.calculateTopDistance()
+        playerView.frame = CGRect(x: 0, y: self.calculateTopDistance(), width: self.view.bounds.width, height: 240)
         playerView.autoresizingMask = .flexibleWidth
-    }
-
-    func loadWebView() {
-        if let youtubeId = resource?.videoEmbed.components(separatedBy: "/").last {
-            playerView.videoId = youtubeId
-        }
     }
 
 }
